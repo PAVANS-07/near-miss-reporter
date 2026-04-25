10db95befa30c62b24decb47dab10730d1c6f2b1
unknown <pavans8105@gmail.com>
2026-04-25 11:32:03 +0530

    Day2 backend

---
+++ backend/src/main/java/com/internship/tool/service/ReportService.java
@@ -0,0 +1,19 @@
+package com.internship.tool.service;
+
+import com.internship.tool.entity.Report;
+import com.internship.tool.repository.ReportRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ReportService {
+
+    @Autowired
+    private ReportRepository repo;
+
+    public List<Report> getAllReports() {
+        return repo.findAll();
+    }
+}
